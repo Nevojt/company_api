2a96046ff3bd5a50e468841eb0c9aee24cf1b328
Nevojt <nevojt@gmail.com>
2024-07-23 21:03:11 +0200

    test connection database

--- app/database/database.py
+++ app/database/database.py
@@ -33,8 +33,8 @@ async def get_db():
        
 while True:   
     try:
-        conn = psycopg2.connect(host=settings.database_hostname, database=settings.database_name, user=settings.database_username,
-                                password=settings.database_password, cursor_factory=RealDictCursor)
+        conn = psycopg2.connect(host=settings.database_hostname_company, database=settings.database_name, user=settings.database_username,
+                                password=settings.database_password_company, cursor_factory=RealDictCursor)
         cursor = conn.cursor()
         print("Database connection was successful")
         break
