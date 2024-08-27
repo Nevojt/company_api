6049ac65dba26e341ca5570ef5cc8f12e4a537ca
Nevojt <nevojt@gmail.com>
2024-08-27 20:56:33 +0200

    created functionality add following users

--- app/models/messages_model.py
+++ app/models/messages_model.py
@@ -1,11 +1,9 @@
-from datetime import timedelta
-from tkinter import CASCADE
-from sqlalchemy import JSON, Column, Integer, Interval, String, Boolean, ForeignKey, Enum, DateTime
+
+# from tkinter import CASCADE
+from sqlalchemy import JSON, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.orm import relationship
 
-from enum import Enum as PythonEnum
 from app.database.database import Base
 
 
