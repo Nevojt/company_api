839fb4747ac2ef130e79167fd7c958d42498a9bb
Nevojt <nevojt@gmail.com>
2024-08-27 18:50:50 +0200

    update method return message

--- app/routers/messages/message.py
+++ app/routers/messages/message.py
@@ -46,63 +46,7 @@ async def async_decrypt(encoded_data: str):
     except InvalidToken:
         return None
 
-@router.get("/", response_model=List[message.SocketModel], include_in_schema=False)
-async def get_posts(session: AsyncSession = Depends(get_async_session), 
-                    limit: int = 50, skip: int = 0):
-    
-    """
-    Retrieves a list of socket messages with associated user details, paginated by a limit and offset.
 
-    Args:
-        session (AsyncSession, optional): Asynchronous database session. Defaults to Depends(get_async_session).
-        limit (int, optional): Maximum number of messages to retrieve. Defaults to 50.
-        skip (int, optional): Number of messages to skip for pagination. Defaults to 0.
-
-    Returns:
-        List[schemas.SocketModel]: A list of socket messages along with user details, structured as per SocketModel schema.
-    """
-
-    query = select(
-        messages_model.Socket, 
-        user_model.User, 
-        func.coalesce(func.sum(messages_model.Vote.dir), 0).label('votes')
-    ).outerjoin(
-        messages_model.Vote, messages_model.Socket.id == messages_model.Vote.message_id
-    ).join(
-        user_model.User, messages_model.Socket.receiver_id == user_model.User.id
-    ).group_by(
-        messages_model.Socket.id, user_model.User.id
-    ).order_by(
-        desc(messages_model.Socket.created_at)
-    ).limit(50)
-
-    result = await session.execute(query)
-    raw_messages = result.all()
-
-    result = await session.execute(query)
-    raw_messages = result.all()
-
-    # Convert raw messages to SocketModel
-    messages = []
-    for socket, user, votes in raw_messages:
-        decrypted_message = await async_decrypt(socket.message)
-        messages.append(
-            message.SocketModel(
-                created_at=socket.created_at,
-                receiver_id=socket.receiver_id,
-                message=decrypted_message,
-                fileUrl=socket.fileUrl,
-                user_name=user.user_name if user is not None else "Unknown user",
-                avatar=user.avatar if user is not None else "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/inne/image/photo_2024-06-14_19-20-40.jpg",
-                verified=user.verified if user is not None else None,
-                id=socket.id,
-                vote=votes,
-                id_return=socket.id_return,
-                edited=socket.edited
-            )
-        )
-    messages.reverse()
-    return messages
 
 async def check_room_blocked(room_id: int, session: AsyncSession):
     query = select(room_model.Rooms).where(room_model.Rooms.id == room_id, room_model.Rooms.block.is_(True))
@@ -229,7 +173,8 @@ async def get_messages_room(room_id: int,
                 id=socket.id,
                 vote=votes,
                 id_return=socket.id_return,
-                edited=socket.edited
+                edited=socket.edited,
+                return_message=socket.return_message
             )
         )
     messages.reverse()
@@ -258,4 +203,63 @@ async def change_message(id_message: int, message_update: message.SocketUpdate,
     return {"message": "Message updated successfully"}
     
 
-        
+
+# Old functions
+
+@router.get("/", response_model=List[message.SocketModel], include_in_schema=False)
+async def get_posts(session: AsyncSession = Depends(get_async_session), 
+                    limit: int = 50, skip: int = 0):
+    
+    """
+    Retrieves a list of socket messages with associated user details, paginated by a limit and offset.
+
+    Args:
+        session (AsyncSession, optional): Asynchronous database session. Defaults to Depends(get_async_session).
+        limit (int, optional): Maximum number of messages to retrieve. Defaults to 50.
+        skip (int, optional): Number of messages to skip for pagination. Defaults to 0.
+
+    Returns:
+        List[schemas.SocketModel]: A list of socket messages along with user details, structured as per SocketModel schema.
+    """
+
+    query = select(
+        messages_model.Socket, 
+        user_model.User, 
+        func.coalesce(func.sum(messages_model.Vote.dir), 0).label('votes')
+    ).outerjoin(
+        messages_model.Vote, messages_model.Socket.id == messages_model.Vote.message_id
+    ).join(
+        user_model.User, messages_model.Socket.receiver_id == user_model.User.id
+    ).group_by(
+        messages_model.Socket.id, user_model.User.id
+    ).order_by(
+        desc(messages_model.Socket.created_at)
+    ).limit(50)
+
+    result = await session.execute(query)
+    raw_messages = result.all()
+
+    result = await session.execute(query)
+    raw_messages = result.all()
+
+    # Convert raw messages to SocketModel
+    messages = []
+    for socket, user, votes in raw_messages:
+        decrypted_message = await async_decrypt(socket.message)
+        messages.append(
+            message.SocketModel(
+                created_at=socket.created_at,
+                receiver_id=socket.receiver_id,
+                message=decrypted_message,
+                fileUrl=socket.fileUrl,
+                user_name=user.user_name if user is not None else "Unknown user",
+                avatar=user.avatar if user is not None else "https://tygjaceleczftbswxxei.supabase.co/storage/v1/object/public/image_bucket/inne/image/photo_2024-06-14_19-20-40.jpg",
+                verified=user.verified if user is not None else None,
+                id=socket.id,
+                vote=votes,
+                id_return=socket.id_return,
+                edited=socket.edited
+            )
+        )
+    messages.reverse()
+    return messages
