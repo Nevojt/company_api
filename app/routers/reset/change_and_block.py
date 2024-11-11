d019905db375fcbcf144bc5e36c160ffe156384d
dmnevoit <dnevoit@sayorama.eu>
2024-11-11 20:37:54 +0100

    Update reset password

--- app/routers/reset/change_and_block.py
+++ app/routers/reset/change_and_block.py
@@ -1,8 +1,9 @@
+from _log_config.log_config import get_logger
 from datetime import datetime
 from fastapi import status, HTTPException, Depends, APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from fastapi import BackgroundTasks
 import pytz
-from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.mail import send_mail
@@ -12,6 +13,9 @@ from ...auth import oauth2
 from ...database.async_db import get_async_session
 from app.models import user_model
 from app.schemas import user
+from app.settings.get_info import get_user, has_verified_or_blocked_user
+
+logger = get_logger('manipulation', 'change_and_block.log')
 
 router = APIRouter(
     prefix="/manipulation",
@@ -20,7 +24,8 @@ router = APIRouter(
 
 
 @router.put("/password", response_description="Reset password")
-async def reset(password: user.UserUpdatePassword,
+async def reset(background_tasks: BackgroundTasks,
+                password: user.UserUpdatePassword,
                 db: AsyncSession = Depends(get_async_session),
                 current_user: user_model.User = Depends(oauth2.get_current_user)):
     """
@@ -39,48 +44,55 @@ async def reset(password: user.UserUpdatePassword,
     HTTPException: If the user is not found.
     HTTPException: If the old password is incorrect.
     """
-    if current_user.verified == False or current_user.blocked:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail=f"User with ID {current_user.id} is not verified or blocked")
-        
-    query = select(user_model.User).where(user_model.User.id == current_user.id)
-    result = await db.execute(query)
-    existing_user = result.scalar_one_or_none()
-    
-    if not current_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    
-    if not utils.verify(password.old_password, existing_user.password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect password."
-        )
-  
-    current_time_utc = datetime.now(pytz.UTC)
-    # hashed new password
-    hashed_password = utils.hash(password.new_password)
+    try:
+        if await has_verified_or_blocked_user(current_user):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                                detail=f"User with ID {current_user.id} is not verified or blocked")
 
-    # Update password to database
-    current_user.password = hashed_password
-    current_user.password_changed = current_time_utc
-    db.add(current_user)
-    await db.commit()
-    
-    token = current_user.refresh_token
-    blocked_link = f"https://{settings.url_address_dns_company}/api/manipulation/blocked?token={token}"
-    await send_mail.send_mail_for_change_password("Changing your account password", current_user.email,
-            {
-                "title": "Changing your account password",
-                "name": current_user.user_name,
-                "blocked_link": blocked_link
-            }
-        )
+        query_user = await get_user(current_user.id, db)
 
-    return {"msg": "Password has been reset successfully."}
+        if not current_user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+        if not utils.verify(password.old_password, query_user.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect password."
+            )
+
+        current_time_utc = datetime.now(pytz.UTC)
+        # hashed new password
+        hashed_password = utils.hash(password.new_password)
+
+        # Update password to database
+        current_user.password = hashed_password
+        current_user.password_changed = current_time_utc
+        db.add(current_user)
+        await db.commit()
+
+        token = current_user.refresh_token
+        blocked_link = f"https://{settings.url_address_dns_company}/api/manipulation/blocked?token={token}"
+
+        background_tasks.add_task(send_mail.send_mail_for_change_password,
+                                  "Changing your account password",
+                                  current_user.email,
+                                  {
+                                      "title": "Changing your account password",
+                                      "name": current_user.user_name,
+                                      "blocked_link": blocked_link
+                                  })
+
+        return {"msg": "Password has been reset successfully."}
+
+    except Exception as e:
+        logger.error(f"An error occurred while resetting password: {e}", exc_info=True)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=str(e))
 
 templates = Jinja2Templates(directory="templates")
 @router.get("/blocked")
-async def block_account(token: str, request: Request, db: AsyncSession = Depends(get_async_session)):
+async def block_account(token: str, request: Request,
+                        db: AsyncSession = Depends(get_async_session)):
     """
     This function is responsible for blocking a user's account.
     It retrieves the user from the database using the provided token.
@@ -98,13 +110,17 @@ async def block_account(token: str, request: Request, db: AsyncSession = Depends
     Raises:
     HTTPException: If the user is not found in the database.
     """
-    user = await oauth2.get_current_user(token, db)
-    
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    
-    user.blocked = True
-    db.add(user)
-    await db.commit()
-    
-    return templates.TemplateResponse("blocked_account.html", {"request": request})
+    try:
+        get_user = await oauth2.get_current_user(token, db)
+
+        if not get_user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+        get_user.blocked = True
+        db.add(get_user)
+        await db.commit()
+
+        return templates.TemplateResponse("blocked_account.html", {"request": request})
+    except Exception as e:
+        logger.error(f"An error occurred while blocking user: {e}", exc_info=True)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
