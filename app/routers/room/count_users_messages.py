de43d9fa5554070082d79512c2713a7bcc2c2dee
dmnevoit <dnevoit@sayorama.eu>
2024-11-11 20:01:23 +0100

    Update count users and rooms

--- app/routers/room/count_users_messages.py
+++ app/routers/room/count_users_messages.py
@@ -1,10 +1,19 @@
+
+from _log_config.log_config import get_logger
 from fastapi import status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 from sqlalchemy import func
+from sqlalchemy.future import select
 from typing import List
-from app.database.database import get_db
+
+from app.database.async_db import get_async_session
 from app.models import messages_model, user_model
 from app.schemas import room
+from app.config.start_schema import start_app
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+logger = get_logger('count', 'count_users_messages.log')
 
 router = APIRouter(
     prefix="/count",
@@ -12,9 +21,10 @@ router = APIRouter(
     )
 
 
+hell = start_app.default_room_name
 
 @router.get("/messages", response_model=List[room.CountMessages])
-async def get_count_messages(db: Session = Depends(get_db)):
+async def get_count_messages(db: AsyncSession = Depends(get_async_session)):
     """
     Get the count of messages in each room.
 
@@ -27,20 +37,26 @@ async def get_count_messages(db: Session = Depends(get_db)):
     Raises:
         HTTPException: If no messages found.
     """
-    query_result = db.query(messages_model.ChatMessages.rooms, func.count(messages_model.ChatMessages.id).label('count')).group_by(
-        messages_model.ChatMessages.rooms).filter(messages_model.ChatMessages.rooms != 'Hell').all()
-    
-    if not query_result:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="No messages found in any room.")
-    counts = [{"rooms": rooms, "count": count} for rooms, count in query_result]
+    try:
+        query_result = await db.execute(select(
+            messages_model.ChatMessages.rooms, func.count(messages_model.ChatMessages.id).label('count')).group_by(
+            messages_model.ChatMessages.rooms).filter(messages_model.ChatMessages.rooms != hell))
+        result = query_result.scalars().all()
 
-    return counts
+        if not result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="No messages found in any room.")
+        counts = [{"rooms": rooms, "count": count} for rooms, count in query_result]
+
+        return counts
+    except Exception as e:
+        logger.error(f"Error in get_count_messages: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
 
 @router.get("/users", response_model=List[room.CountUsers])
-async def get_count_users(db: Session = Depends(get_db)):
+async def get_count_users(db: AsyncSession = Depends(get_async_session)):
     """
     Get the count of users in each room.
 
@@ -53,12 +69,18 @@ async def get_count_users(db: Session = Depends(get_db)):
     Raises:
         HTTPException: If no users found.
     """
-    query_result = db.query(user_model.UserStatus.name_room, func.count(user_model.UserStatus.id).label('count')).group_by(
-        user_model.UserStatus.name_room).filter(user_model.UserStatus.name_room != 'Hell').all()
-    
-    if not query_result:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="No users found in any room.")
-    counts = [{"rooms": name_room, "count": count} for name_room, count in query_result]
-    
-    return counts
+    try:
+        query_result = await db.execute(select(
+            user_model.UserStatus.name_room, func.count(user_model.UserStatus.id).label('count')).group_by(
+            user_model.UserStatus.name_room).filter(user_model.UserStatus.name_room != hell))
+        result = query_result.scalars().all()
+
+        if not result:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail="No users found in any room.")
+        counts = [{"rooms": name_room, "count": count} for name_room, count in query_result]
+
+        return counts
+    except Exception as e:
+        logger.error(f"Error in get_count_users: {e}")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
